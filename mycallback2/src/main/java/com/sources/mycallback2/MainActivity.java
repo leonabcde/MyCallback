b337b58b1ca1497739182555761539a5c08d0d9e
Handsome-Chen <leonabcde@163.com>
2017-07-05 01:09:25 +0800

    eventbus终于有些熟悉了，为了使两边的数据保持同步，使用了eventbus，和全局变量，不伦不类，暂时凑合吧

--- mycallback2/src/main/java/com/sources/mycallback2/MainActivity.java
+++ mycallback2/src/main/java/com/sources/mycallback2/MainActivity.java
@@ -3,12 +3,19 @@ package com.sources.mycallback2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.sources.mycallback2.util.Utils;
 
 import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,49 +26,70 @@ public class MainActivity extends AppCompatActivity {
     public CheckBox checkbox2;
     @InjectView(R.id.CheckBox3)
     public CheckBox checkbox3;
+    @InjectView(R.id.tv1)
+    public TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.inject(this);
         // 注册eventbus
-        EventBus.getDefault().register(MainActivity.this);
+        EventBus.getDefault().register(this);
 
-        initView();
+        changeState(Utils.STATE);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //解注册eventbus
-        EventBus.getDefault().unregister(MainActivity.this);
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void checkEventBus(EventBusMessage message){
+        Log.e("接收： ",""+message.getMessage());
+        tv1.setText("接收到的状态值为： " + message.getMessage());
+
+        checkbox1.setChecked((4 & message.getMessage()) == 4);
+        checkbox2.setChecked((2 & message.getMessage()) == 2);
+        checkbox3.setChecked((1 & message.getMessage()) == 1);
+
+//        Utils.STATE = message.getMessage();
     }
 
-    /**
-     * 点击跳转到第二个页面
-     *
-     * @param v
-     */
     public void buttonClick(View v) {
-        EventBus.getDefault().post(new EventBusMessage("第一个页面发送过来的信息"));
 
         Intent intent = new Intent(MainActivity.this, CallActivity.class);
         startActivity(intent);
     }
 
-    public void initView() {
-        Toast.makeText(MainActivity.this, "dyhghjgy", Toast.LENGTH_SHORT).show();
+    private void changeState(final int state){
+        checkbox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (checkbox1.isChecked()) Utils.STATE = Utils.STATE | 4;
+                else Utils.STATE = Utils.STATE & 3;
+            }
+        });
+
+        checkbox2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (checkbox2.isChecked()) Utils.STATE = Utils.STATE | 2;
+                else Utils.STATE = Utils.STATE & 5;
+            }
+        });
+
+        checkbox3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (checkbox3.isChecked()) Utils.STATE = Utils.STATE | 1;
+                else Utils.STATE = Utils.STATE & 6;
+            }
+        });
     }
 
-
-    private CallbackListener mCallbackListener;
-
-    public void getCallbackListener(CallbackListener listener) {
-        this.mCallbackListener = listener;
-    }
-
-    public interface CallbackListener {
-        void callback();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("Main onDestroy： ","Main onDestroy");
+        // 取消注册
+        EventBus.getDefault().unregister(this);
     }
 }
